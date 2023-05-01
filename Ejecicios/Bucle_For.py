cf3c7f6875f3d478e098277e0c8f12fc5c233ae4
CARLOSIJM <carlosidjmdz@gmail.com>
2023-05-01 09:53:48 -0600

    Carga inicial de curso de python

---
+++ Ejecicios/Bucle_For.py
@@ -0,0 +1,9 @@
+#frutas = ['manzana','pera','mango']
+#for fruta in frutas:
+#    print(fruta)
+
+iter('cadena') # cadena
+iter(['a','b','c']) # lista
+iter(('a','b','c')) # tupla
+iter({'a','b','c'}) # conjunto
+iter({'a':1,'b':2,'c':3}) #Diccionario
---
+++ Ejecicios/Calculadora_raiz.py
@@ -0,0 +1,5 @@
+def enumeracion_exhaustiva (objetivo):
+    respuesta = 0
+
+    while respuesta**2 <objetivo:
+        respuesta +=1
